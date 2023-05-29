1b65b16683a06c52cc9d114a80601efb0b4950e4
Tomas Vemola <tomas@lun.energy>
2023-05-29 14:20:44 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["./*.{html,js}"],
+    theme: {
+        fontFamily: {
+            serif: ["Lora", "serif"],
+            sans: ["Source Sans Pro", "sans"],
+        },
+        colors: {
+            "off-black": "#212629",
+            "off-white": "#f2f2f2",
+            "accent-blue": "#3f88c5",
+            "accent-red": "#d33f49",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
