876b2bc273541fc083f416a8fbb82489147f5b92
vemolista <24554998+vemolista@users.noreply.github.com>
2023-09-01 16:38:13 +1000

    improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,6 @@ module.exports = {
             serif: ["Lora", "serif"],
             sans: ["Source Sans Pro", "sans"],
         },
-        colors: {
-            "off-black": "#212629",
-            "off-white": "#f2f2f2",
-            "accent-blue": "#3f88c5",
-            "accent-red": "#d33f49",
-        },
         extend: {},
     },
     plugins: [],
